cda611aef0c78f94f9f4c1afe055a6981c8219fd
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-04-06 15:27:24 -0300

    feat: add page not found

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+  selector: 'app-not-found',
+  imports: [
+    HeaderComponent,
+    FooterComponent
+],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css',
+})
+export class NotFoundComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
